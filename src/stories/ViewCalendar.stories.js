6e9841dcde89aafcf8eedc0af4ba172b150be063
Maria Angella <maria5angella@gmail.com>
2020-04-07 09:53:49 +0300

    viewcalendar for volunteer

---
+++ src/stories/ViewCalendar.stories.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const currentDate = '2018-11-01';
+const schedulerData = [
+  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+];
+
+export default {
+    title: 'ViewCalendar'
+  };
+
+export const Calendar = () => (
+  <Paper>
+    <Scheduler
+      data={schedulerData}
+    >
+      <ViewState
+        currentDate={currentDate}
+      />
+      <DayView
+        startDayHour={9}
+        endDayHour={14}
+      />
+      <Appointments />
+    </Scheduler>
+  </Paper>
+);
