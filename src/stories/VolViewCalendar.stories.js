9bdad36f4ddb937055fc9478f6679c08c493f1cb
Maria Angella <maria5angella@gmail.com>
2020-04-08 17:25:20 +0300

    Ref-080

--- src/stories/VolViewCalendar.stories.js
+++ src/stories/VolViewCalendar.stories.js
@@ -7,26 +7,34 @@ import {
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
+
+
+
+
 const currentDate = '2020-04-07';
 const schedulerData = [
   { startDate: '2020-04-07T09:45', endDate: '2020-04-07T11:00', title: 'Cleaning the church' },
-  { startDate: '2020-04-07T12:00', endDate: '2020-04-07T13:30', title: 'Setting up the cameras' },
-  { startDate: '2020-04-08T03:45', endDate: '2020-04-08T04:00', title: 'Cleaning the church' },
+  { startDate: '2020-04-07T12:00', endDate: '2020-04-07T15:30', title: 'Setting up the cameras' },
+  { startDate: '2020-04-08T03:45', endDate: '2020-04-08T06:00', title: 'Cleaning the church' },
   { startDate: '2020-04-09T12:00', endDate: '2020-04-09T13:30', title: 'Setting up the cameras' },
-  { startDate: '2020-04-10T09:45', endDate: '2020-04-10T11:00', title: 'Cleaning the church' },
-  { startDate: '2020-04-11T12:00', endDate: '2020-04-11T13:30', title: 'Setting up the cameras' },
+  { startDate: '2020-04-10T04:45', endDate: '2020-04-10T07:00', title: 'Cleaning the church' },
+  { startDate: '2020-04-11T01:00', endDate: '2020-04-11T03:30', title: 'Setting up the cameras' },
 ];
 
 export default {
-    title: 'ViewCalendar'
+    title: 'VolViewCalendar'
   };
+  
   export const Calendar = () => (
   <Paper>
     <Scheduler
       data={schedulerData}
+    
     >
+      
       <ViewState
         currentDate={currentDate}
+       
       />
       <WeekView
         startDayHour={1}
