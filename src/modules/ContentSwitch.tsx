39b7408890263da9e70fd5f4cd152b3d4c81e8ad
ahmedshayo26 <ahmedshayo014@gmail.com>
2020-04-24 15:54:57 +0300

    WIP designs for team lead calendar and ream lead dashboard

--- src/modules/ContentSwitch.tsx
+++ src/modules/ContentSwitch.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import {Link, Route, Switch} from 'react-router-dom'
-import {localRoutes} from "../data/constants";
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import { localRoutes } from "../data/constants";
 import Dashboard from "./dashboard/Dashboard";
 import Contacts from "./contacts/Contacts";
 import ContactDetails from "./contacts/details/Details";
@@ -9,27 +9,33 @@ import Layout from "../components/layout/Layout";
 import Groups from "./groups/GroupsList";
 import Users from "./admin/users/Users";
 import UserGroups from "./admin/usergroups/UserGroups";
-
+import TeamLeadCalendar from "./teamlead/teamleadcalendar/teamleadcalendar";
+import TeamLeadDashboard from "./teamlead/teamleaddashboard/teamleadDashboard";
 
 const ContentSwitch = () => {
-    return <Switch>
-        <Route exact={true} path="/" component={Dashboard}/>
-        <Route path={localRoutes.dashboard} component={Dashboard}/>
-        <Route path={localRoutes.contactsDetails} component={ContactDetails}/>
-        <Route path={localRoutes.contacts} component={Contacts}/>
-        <Route path={localRoutes.users} component={Users}/>
-        <Route path={localRoutes.usersGroups} component={UserGroups}/>
-        <Route path={localRoutes.groups} component={Groups}/>
-        <Route path={localRoutes.settings} component={Settings}/>
-        <Route component={NoMatch}/>
+  return (
+    <Switch>
+      <Route exact={true} path="/" component={Dashboard} />
+      <Route path={localRoutes.dashboard} component={Dashboard} />
+      <Route path={localRoutes.contactsDetails} component={ContactDetails} />
+      <Route path={localRoutes.contacts} component={Contacts} />
+      <Route path={localRoutes.users} component={Users} />
+      <Route path={localRoutes.usersGroups} component={UserGroups} />
+      <Route path={localRoutes.groups} component={Groups} />
+      <Route path={localRoutes.settings} component={Settings} />
+      <Route path={localRoutes.teamleadcalendar} component={TeamLeadCalendar} />
+      <Route path={localRoutes.teamleaddashboard}component={TeamLeadDashboard} />
+            
+      <Route component={NoMatch} />
     </Switch>
-}
+  );
+};
 
 const NoMatch = () => (
-    <Layout>
-        <h2>Oops nothing here!!</h2>
-        <Link to="/">Take me home</Link>
-    </Layout>
-)
+  <Layout>
+    <h2>Oops nothing here!!</h2>
+    <Link to="/">Take me home</Link>
+  </Layout>
+);
 
-export default ContentSwitch
+export default ContentSwitch;
---
+++ src/modules/teamlead/teamleaddashboard/teamleadDashboard.js
@@ -0,0 +1,176 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import Card from "@material-ui/core/Card";
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));
+
+export default function PersistentDrawerLeft() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Team Lead Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+          {["Dashboard", "Calendar", "Volunteers", "Drafts"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {["Register Task", "View Tasks", "Assign Tasks"].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <main
+        className={clsx(classes.content, {
+          [classes.contentShift]: open,
+        })}
+      >
+        <div className={classes.drawerHeader} />
+        <div>
+          <Card className={classes.root} variant="outlined">
+            <Typography variant="h1">
+            <h1>Welcome to your dashboard</h1>
+            </Typography>
+          </Card>
+        </div>        
+      </main>
+    </div>
+  );
+}
