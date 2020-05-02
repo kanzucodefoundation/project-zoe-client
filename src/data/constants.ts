33d39ba76d307acb7b5f7e84bc877546e68e74f9
ibia-byte <retyibea@yahoo.com>
2020-05-02 13:30:45 +0300

    created reducer file

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,78 +1,83 @@
-export const AUTH_TOKEN_KEY = '__demo__eva__token'
-export const AUTH_USER_KEY = '__demo__eva__user'
-
+export const AUTH_TOKEN_KEY = "__demo__eva__token";
+export const AUTH_USER_KEY = "__demo__eva__user";
 
 export const appRoles = {
-    roleCrmView: "CRM_VIEW",
-    roleCrmEdit: "CRM_EDIT",
+  roleCrmView: "CRM_VIEW",
+  roleCrmEdit: "CRM_EDIT",
 
-    roleAuthUserView: "AUTH_USER_VIEW",
-    roleAuthUserEdit: "AUTH_USER_EDIT",
+  roleAuthUserView: "AUTH_USER_VIEW",
+  roleAuthUserEdit: "AUTH_USER_EDIT",
 
-    roleAuthGroupView: "AUTH_GROUP_VIEW",
-    roleAuthGroupEdit: "AUTH_GROUP_EDIT",
+  roleAuthGroupView: "AUTH_GROUP_VIEW",
+  roleAuthGroupEdit: "AUTH_GROUP_EDIT",
 
-    roleTagView: "AUTH_TAG_VIEW",
-    roleTagEdit: "AUTH_TAG_EDIT",
+  roleTagView: "AUTH_TAG_VIEW",
+  roleTagEdit: "AUTH_TAG_EDIT",
 
-    roleGroupView: "GROUP_VIEW",
-    roleGroupEdit: "GROUP_EDIT",
-}
-
+  roleGroupView: "GROUP_VIEW",
+  roleGroupEdit: "GROUP_EDIT",
+};
 
 export const redux = {
-    doLogin: 'DO_LOGIN',
-    doLogout: 'DO_LOGOUT',
-    doSearch: 'DO_SEARCH',
+  doLogin: "DO_LOGIN",
+  doLogout: "DO_LOGOUT",
+  doSearch: "DO_SEARCH",
 };
 
 export const localRoutes = {
+  dashboard: "/dashboard",
+  contacts: "/people/contacts",
+  contactsDetails: "/people/contacts/:contactId",
+  groups: "/people/groups",
 
-    dashboard: '/dashboard',
-    contacts: '/people/contacts',
-    contactsDetails: '/people/contacts/:contactId',
-    groups: '/people/groups',
+  users: "/admin/users",
+  usersGroups: "/admin/user-groups",
+  tags: "/admin/tags",
+  settings: "/admin/settings",
+  volcalendar: "/volcalendar",
+  voldashboard: "/voldashboard",
+  volblockdate: "/volblockdate",
+  volviewteam: "/volviewteam",
 
-    users: '/admin/users',
-    usersGroups: '/admin/user-groups',
-    tags: '/admin/tags',
-    settings: '/admin/settings',
+  help: "/help",
 
-    help: '/help'
-}
+  // For Team Lead
+  viewVolunteers: "/volunteers/view-volunteers",
+  addVolunteers: "/volunteers/add-volunteers",
+  addTasks: "/tasks/add-tasks",
+  viewTasks:"/tasks/view-tasks"
+};
 
-const debug = process.env.NODE_ENV !== 'production'
-export const url = debug ? 'http://localhost:4002' :
-    'http://hgjyuk.com/server'
+const debug = process.env.NODE_ENV !== "production";
+export const url = debug ? "http://localhost:4002" : "http://hgjyuk.com/server";
 
 export const remoteRoutes = {
-    authServer: url,
-    login: url + '/api/auth/login',
-    profile: url + '/api/auth/profile',
-    register: url + '/api/auth/register',
-    resetPass: url + '/reset',
-    contacts: url + '/api/crm/contacts',
-    contactSearch: url + '/api/crm/contact/search',
-    contactById: url + '/api/crm/contacts/id',
-    contactsPerson: url + '/api/crm/people',
-    contactsChc: url + '/api/crm/person/chc',
-    contactsEmail: url + '/api/crm/emails',
-    tags: url + '/api/tags',
-    users: url + '/api/users',
-    userGroups: url + '/api/user-groups',
-    contactsPhone: url + '/api/crm/phones',
-    contactsAddress: url + '/api/crm/addresses',
-    contactsIdentification: url + '/api/crm/identifications',
-    contactsRequests: url + '/api/crm/requests',
+  authServer: url,
+  login: url + "/api/auth/login",
+  profile: url + "/api/auth/profile",
+  register: url + "/api/auth/register",
+  resetPass: url + "/reset",
+  contacts: url + "/api/crm/contacts",
+  contactSearch: url + "/api/crm/contact/search",
+  contactById: url + "/api/crm/contacts/id",
+  contactsPerson: url + "/api/crm/people",
+  contactsChc: url + "/api/crm/person/chc",
+  contactsEmail: url + "/api/crm/emails",
+  tags: url + "/api/tags",
+  users: url + "/api/users",
+  userGroups: url + "/api/user-groups",
+  contactsPhone: url + "/api/crm/phones",
+  contactsAddress: url + "/api/crm/addresses",
+  contactsIdentification: url + "/api/crm/identifications",
+  contactsRequests: url + "/api/crm/requests",
 
-    groups: url + '/api/groups/groups',
-    groupsCombo: url + '/api/groups/combo',
-    groupsCategories: url + '/api/groups/categories',
+  groups: url + "/api/groups/groups",
+  groupsCombo: url + "/api/groups/combo",
+  groupsCategories: url + "/api/groups/categories",
 
-    contactsCompany: url + '/api/crm/contact/company',
-    contactsAvatar: url + '/api/crm/contact/avatar',
+  contactsCompany: url + "/api/crm/contact/company",
+  contactsAvatar: url + "/api/crm/contact/avatar",
 
-}
-
-
-
+  volunteers: url + "/api/services/volunteers",
+  tasks: url + "/api/tasks",
+};
---
+++ src/data/tasks/reducer.ts
@@ -0,0 +1,44 @@
+import { ICreateTaskDto } from "../../modules/tasks/types";
+
+export const servicesConstants = {
+  servicesFetchAll: "servicesFetchAll",
+  servicesFetchLoading: "servicesFetchLoading",
+
+  servicesAddTask: "servicesAddTask",
+
+  coreLogout: "CORE_LOGOUT",
+};
+
+export interface IServicesState {
+  data: ICreateTaskDto[];
+  selected?: ICreateTaskDto;
+  loading: boolean;
+}
+
+const initialState: IServicesState = {
+  data: [],
+  loading: true,
+  selected: undefined,
+};
+
+export default function reducer(state = initialState, action: any) {
+  switch (action.type) {
+    case servicesConstants.servicesFetchAll: {
+      const data: ICreateTaskDto[] = action.payload;
+      return { ...state, data, loading: false };
+    }
+
+    case servicesConstants.servicesFetchLoading: {
+      return { ...state, loading: action.payload };
+    }
+
+    case servicesConstants.servicesAddTask: {
+      const newTask: ICreateTaskDto[] = action.payload;
+      return { ...state, data: [...state.data, newTask] };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
