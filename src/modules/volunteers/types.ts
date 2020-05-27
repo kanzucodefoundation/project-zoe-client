fa8fbe33986622c5f4483f4ed65c12dd4c7c9e85
danieljbuyinza <danieljbuyinza@gmail.com>
2020-05-27 12:06:44 +0300

    Resolved error on Add Volunteer and View Tasks

--- src/modules/volunteers/types.ts
+++ src/modules/volunteers/types.ts
@@ -1,16 +1,7 @@
-import { CivilStatus } from "../contacts/types";
-
 export interface ICreateAVolunteerDto {
     category: string;
     ministry: string;
     firstName: string;
     lastName: string;
-    gender: string,
-    civilStatus: CivilStatus;
-    phone: string,
-    email: string;
     password: string;
-    dateOfBirth: string;
-    missionalCommunity: string;
-    profession: string;
 }
