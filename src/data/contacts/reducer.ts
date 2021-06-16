f6ea8f3aa8a5899388d74864503af578b373a237
Herbert Otim <herbert.otim@gmail.com>
2021-06-16 09:30:30 +0200

    Fix-004: As an admin, I'd like an error on adding user's new phone number fixed on user details.

--- src/data/contacts/reducer.ts
+++ src/data/contacts/reducer.ts
@@ -108,10 +108,11 @@ export default function reducer(state = initialState, action: any) {
             return state
         }
         case crmConstants.crmAddPhone: {
-            const phone: IPhone = action.payload
+            const phone: IPhone[] = action.payload
+            const newPhone: IPhone[] = []
             if (state.selected) {
                 const {phones, ...rest} = state.selected
-                const selected: IContact = {...rest, phones: [...phones, phone]}
+                const selected: IContact = {...rest, phones: [...phone]}
                 return {...state, selected}
             }
             return state
--- src/modules/contacts/details/info/Phones.tsx
+++ src/modules/contacts/details/info/Phones.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { IContact, IEmail } from "../../types";
+import { IContact, IEmail} from "../../types";
 import { createStyles, makeStyles, Theme } from "@material-ui/core";
 import EditDialog from "../../../../components/EditDialog";
 import PhoneEditor from "../editors/PhoneEditor";
@@ -10,70 +10,93 @@ import DataCard from "../../../../components/DataCard";
 import Button from "@material-ui/core/Button";
 import Hidden from "@material-ui/core/Hidden";
 import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+import Toast from "../../../../utils/Toast";
 
 interface IProps {
-    data: IContact
+  data: IContact;
 }
 
 const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            padding: theme.spacing(2),
-            borderRadius: 0
-        },
-        noPadding: {
-            padding: 0
-        }
-    })
+  createStyles({
+    root: {
+      padding: theme.spacing(2),
+      borderRadius: 0,
+    },
+    noPadding: {
+      padding: 0,
+    },
+  })
 );
 
 const Phones = (props: IProps) => {
-    const classes = useStyles()
-    const [selected, setSelected] = useState<IEmail | null>(null)
-    const [dialog, setDialog] = useState(false)
+  const classes = useStyles();
+  const [selected, setSelected] = useState<IEmail | null>(null);
+  const [dialog, setDialog] = useState(false);
 
-    const handleClick = (email: IEmail) => () => {
-        setSelected(email)
-        setDialog(true)
-    }
+  const handleClick = (email: IEmail) => () => {
+    setSelected(email);
+    setDialog(true);
+  };
 
-    const handleClose = () => {
-        setDialog(false)
-        setSelected(null)
-    }
+  const handleClose = () => {
+    setDialog(false);
+    setSelected(null);
+  };
 
-    const handleNew = () => {
-        setSelected(null)
-        setDialog(true)
-    }
+  const handleNew = () => {
+    setSelected(null);
+    setDialog(true);
+  };
 
-    const {phones, id = ''} = props.data
+  const { phones, id = "" } = props.data;
 
-    return (
-        <DataCard
-            useActionContent={true}
-            title='Phones'
-            buttons={
-                <Button size="small" color="primary" onClick={handleNew}>
-                    Add New
-                </Button>
-            }
+  return (
+    <DataCard
+      useActionContent={true}
+      title="Phones"
+      buttons={
+        <Button
+          size="small"
+          color="primary"
+          onClick={() =>
+            phones.length >= 4
+              ? Toast.info("You can only enter up to four Phone numbers")
+              : handleNew()
+          }
         >
-            <List className={classes.noPadding}>
-                {phones.map(it => (
-                    <ListItem button key={it.id} className={classes.noPadding} onClick={handleClick(it)}>
-                        <ListItemText primary={it.value} secondary={it.category}/>
-                        <Hidden mdUp>
-                            <ArrowRightIcon/>
-                        </Hidden>
-                    </ListItem>
-                ))}
-            </List>
-            <EditDialog title={selected ? "Edit Phone" : "New Phone"} open={dialog} onClose={handleClose}>
-                <PhoneEditor data={selected} isNew={!selected} contactId={id} done={handleClose}/>
-            </EditDialog>
-        </DataCard>
-    )
-}
+          Add New
+        </Button>
+      }
+    >
+      <List className={classes.noPadding}>
+        {phones.map((it) => (
+          <ListItem
+            button
+            key={it.id}
+            className={classes.noPadding}
+            onClick={handleClick(it)}
+          >
+            <ListItemText primary={it.value} secondary={it.category} />
+            <Hidden mdUp>
+              <ArrowRightIcon />
+            </Hidden>
+          </ListItem>
+        ))}
+      </List>
+      <EditDialog
+        title={selected ? "Edit Phone" : "New Phone"}
+        open={dialog}
+        onClose={handleClose}
+      >
+        <PhoneEditor
+          data={selected}
+          isNew={!selected}
+          contactId={id}
+          done={handleClose}
+        />
+      </EditDialog>
+    </DataCard>
+  );
+};
 
 export default Phones;
