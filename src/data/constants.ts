bbe983a3c2933905b7243342993ce0c197c875b6
kiken-eng <kanyecope@gmail.com>
2021-08-06 10:26:29 +0300

    Added a route for join an mc

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,116 +1,118 @@
-export const AUTH_TOKEN_KEY = "__demo__eva__token";
-export const AUTH_USER_KEY = "__demo__eva__user";
+export const AUTH_TOKEN_KEY = '__demo__eva__token';
+export const AUTH_USER_KEY = '__demo__eva__user';
 
 export const appRoles = {
-  roleDashboard: "DASHBOARD",
-  roleCrmView: "CRM_VIEW",
-  roleCrmEdit: "CRM_EDIT",
+  roleDashboard: 'DASHBOARD',
+  roleCrmView: 'CRM_VIEW',
+  roleCrmEdit: 'CRM_EDIT',
 
-  roleUserView: "USER_VIEW",
-  roleUserEdit: "USER_EDIT",
+  roleUserView: 'USER_VIEW',
+  roleUserEdit: 'USER_EDIT',
 
-  roleGroupView: "GROUP_VIEW",
-  roleGroupEdit: "GROUP_EDIT",
+  roleGroupView: 'GROUP_VIEW',
+  roleGroupEdit: 'GROUP_EDIT',
 
-  roleEventView: "EVENT_VIEW",
-  roleEventEdit: "EVENT_EDIT",
+  roleEventView: 'EVENT_VIEW',
+  roleEventEdit: 'EVENT_EDIT',
 };
 
 export const rolesList = Object.values(appRoles);
 
 export const eventsCategories = {
-  garage: "garage",
-  evangelism: "evangelism",
-  wedding: "wedding",
-  baptism: "baptism",
-  mc: "mc",
+  garage: 'garage',
+  evangelism: 'evangelism',
+  wedding: 'wedding',
+  baptism: 'baptism',
+  mc: 'mc',
 };
 
 export const redux = {
-  doLogin: "DO_LOGIN",
-  doLogout: "DO_LOGOUT",
-  doSearch: "DO_SEARCH",
+  doLogin: 'DO_LOGIN',
+  doLogout: 'DO_LOGOUT',
+  doSearch: 'DO_SEARCH',
 };
 
 export const localRoutes = {
-  dashboard: "/dashboard",
-  contacts: "/people/contacts",
-  profile: "/people/contacts/me",
-  contactsDetails: "/people/contacts/:contactId",
-  groups: "/people/groups",
-  groupsDetails: "/people/groups/:groupId",
+  dashboard: '/dashboard',
+  contacts: '/people/contacts',
+  profile: '/people/contacts/me',
+  contactsDetails: '/people/contacts/:contactId',
+  groups: '/people/groups',
+  groupsDetails: '/people/groups/:groupId',
 
-  groupsReports: "/groups",
+  groupsReports: '/groups',
 
-  events: "/events",
-  eventsDetails: "/events/:eventId",
+  events: '/events',
+  eventsDetails: '/events/:eventId',
 
-  reports: "/reports",
-  reportsDetails: "/reports/:reportId",
+  reports: '/reports',
+  reportsDetails: '/reports/:reportId',
 
-  users: "/admin/users",
-  usersGroups: "/admin/user-groups",
-  tags: "/admin/tags",
-  settings: "/admin/settings",
-  test: "/test",
+  users: '/admin/users',
+  usersGroups: '/admin/user-groups',
+  tags: '/admin/tags',
+  settings: '/admin/settings',
+  test: '/test',
 
-  updatePassword: "/update-password",
-  resetPassword: "/reset-password/:token",
-  forgotPassword: "/forgot-password",
-  help: "/help",
-  login: "/login",
-  home: "/",
+  updatePassword: '/update-password',
+  resetPassword: '/reset-password/:token',
+  forgotPassword: '/forgot-password',
+  help: '/help',
+  login: '/login',
+  home: '/',
 };
 
-export const isDebug = process.env.NODE_ENV !== "production";
-const debug = process.env.NODE_ENV !== "production";
+export const isDebug = process.env.NODE_ENV !== 'production';
+const debug = process.env.NODE_ENV !== 'production';
 export const url = debug
-  ? "http://localhost:4002"
-  : "https://app.worshipharvest.org/server";
+  ? 'http://localhost:4002'
+  : 'https://app.worshipharvest.org/server';
 
 export const remoteRoutes = {
   authServer: url,
-  login: url + "/api/auth/login",
-  profile: url + "/api/auth/profile",
-  register: url + "/api/register",
-  forgotPassword: url + "/api/auth/forgot-password",
-  resetPassword: url + "/api/auth/reset-password",
-  contacts: url + "/api/crm/contacts",
-  contactSearch: url + "/api/crm/contact/search",
-  contactById: url + "/api/crm/contacts/id",
-  contactsPeople: url + "/api/crm/people",
-  contactsPeopleCombo: url + "/api/crm/people/combo",
+  login: url + '/api/auth/login',
+  profile: url + '/api/auth/profile',
+  register: url + '/api/register',
+  forgotPassword: url + '/api/auth/forgot-password',
+  resetPassword: url + '/api/auth/reset-password',
+  contacts: url + '/api/crm/contacts',
+  contactSearch: url + '/api/crm/contact/search',
+  contactById: url + '/api/crm/contacts/id',
+  contactsPeople: url + '/api/crm/people',
+  contactsPeopleCombo: url + '/api/crm/people/combo',
 
-  contactsPeopleSample: url + "/api/crm/import",
-  contactsPeopleUpload: url + "/api/crm/import",
-  contactsChc: url + "/api/crm/person/chc",
-  contactsEmail: url + "/api/crm/emails",
-  tags: url + "/api/tags",
-  users: url + "/api/users",
-  userGroups: url + "/api/user-groups",
-  contactsPhone: url + "/api/crm/phones",
-  contactsAddress: url + "/api/crm/addresses",
-  contactsIdentification: url + "/api/crm/identifications",
-  contactsRequests: url + "/api/crm/requests",
+  contactsPeopleSample: url + '/api/crm/import',
+  contactsPeopleUpload: url + '/api/crm/import',
+  contactsChc: url + '/api/crm/person/chc',
+  contactsEmail: url + '/api/crm/emails',
+  tags: url + '/api/tags',
+  users: url + '/api/users',
+  userGroups: url + '/api/user-groups',
+  contactsPhone: url + '/api/crm/phones',
+  contactsAddress: url + '/api/crm/addresses',
+  contactsIdentification: url + '/api/crm/identifications',
+  contactsRequests: url + '/api/crm/requests',
 
-  groups: url + "/api/groups/group",
-  groupsCombo: url + "/api/groups/combo",
-  groupsCategories: url + "/api/groups/category",
-  groupsMembership: url + "/api/groups/member",
-  groupsRequest: url + "/api/groups/request",
-  groupReports: url + "/api/groups/groupreports",
-  groupReportFrequency: url + "/api/groups/reportfrequency",
-  groupCategoriesCombo: url + "/api/groups/groupscombo",
+  groups: url + '/api/groups/group',
+  groupsCombo: url + '/api/groups/combo',
+  groupsCategories: url + '/api/groups/category',
+  groupsMembership: url + '/api/groups/member',
+  groupsRequest: url + '/api/groups/request',
+  groupReports: url + '/api/groups/groupreports',
+  groupReportFrequency: url + '/api/groups/reportfrequency',
+  groupCategoriesCombo: url + '/api/groups/groupscombo',
 
-  events: url + "/api/events/event",
-  eventsMetricsRaw: url + "/api/events/metrics/raw",
-  eventsCategories: url + "/api/events/category",
-  eventsAttendance: url + "/api/events/attendance",
-  eventsField: url + "/api/events/fields",
+  mcRequest: url + '/api/groups/request/mc',
 
-  reports: url + "/api/reports/report",
-  reportsCategories: url + "api/reports/category",
+  events: url + '/api/events/event',
+  eventsMetricsRaw: url + '/api/events/metrics/raw',
+  eventsCategories: url + '/api/events/category',
+  eventsAttendance: url + '/api/events/attendance',
+  eventsField: url + '/api/events/fields',
 
-  contactsCompany: url + "/api/crm/contact/company",
-  contactsAvatar: url + "/api/crm/contact/avatar",
+  reports: url + '/api/reports/report',
+  reportsCategories: url + 'api/reports/category',
+
+  contactsCompany: url + '/api/crm/contact/company',
+  contactsAvatar: url + '/api/crm/contact/avatar',
 };
