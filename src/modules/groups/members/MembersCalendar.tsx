3d3de0b84ae3d372d52f067050a34562ed1f36b9
Jo <joanita.kente@gmail.com>
2021-08-18 18:23:00 +0300

    calendar

--- src/modules/groups/members/MembersCalendar.tsx
+++ src/modules/groups/members/MembersCalendar.tsx
@@ -7,6 +7,7 @@ import "tui-time-picker/dist/tui-time-picker.css";
 import { get } from "../../../utils/ajax";
 import Layout from "../../../components/layout/Layout"
 import { remoteRoutes } from "../../../data/constants";
+import { Alert } from "@material-ui/lab";
 
 const start = new Date();
 const end = new Date(new Date().setMinutes(start.getMinutes() + 30));
@@ -15,12 +16,24 @@ const schedules: ISchedule[] = [
     calendarId: "1",
     category: "time",
     isVisible: true,
-    title: "",
+    title: "Meeting",
     id: "1",
-    body: "",
-    location: "",
+    body: "Description",
+    location: "Kampala",
     start,
     end
+  },
+
+  {
+    calendarId: "2",
+    category: "time",
+    isVisible: true,
+    title: "Community",
+    id: "2",
+    body: "Description",
+    location: "Lugogo",
+    start: new Date(new Date().setHours(start.getHours() + 1)),
+    end: new Date(new Date().setHours(start.getHours() + 2))
   }
 ];
 
@@ -33,56 +46,58 @@ const calendars: ICalendarInfo[] = [
     dragBgColor: "#9e5fff",
     borderColor: "#9e5fff"
   },
+
+  {
+    id: "2",
+    name: "MC Calendar",
+    color: "#ffffff",
+    bgColor: "#00a9ff",
+    dragBgColor: "#00a9ff",
+    borderColor: "#00a9ff"
+  }
   
 ];
 
-interface IEvent {
-    id: number;
-    eventName: string;
-    startDate: Date; 
-    endDate: Date;
-    venue: string;
-  }
-
-const MembersCalendar = (props: any) => {
+const MembersCalendar = () => {
     const [event, setEvent] = useState<any[]>([]);
   
     useEffect(() => {
         get (`${remoteRoutes.events}`,
-        (data) => {let mcEvent: IEvent[] = []; {
-          const groupEvent = {
-            id: data.id,
-            eventName: data.name, 
-            startDate: data.startDate,
-            endDate: data.endDate,
-            venue:data.venue.name,
-          };
-          mcEvent.push(groupEvent);
-        }
-            setEvent(mcEvent);
-    })
+        (data) => { let events: ISchedule[] = [];
+          for (let i = 0; i < data.length; i++) {
+            const mce = {
+              //calendarId: data[i].id,
+              category: "time",
+              isVisible: true,
+              id: data[i].id,
+              title: data[i].name,
+              body: data[i].summary,
+              location: data[i].venue.name,
+              start:data[i].startDate,
+              end:data[i].endDate,
+            };
+  
+            events.push(mce);
+          }
+          console.log(events);
+          setEvent(events);
+          //createSchedules(event);
+          },)
 },
 []);
 
     return (
-    <Layout>
+      <Layout>
         <h1>Worship Harvest Calendar</h1>
-        {event.map((events:ISchedule, index) => (
-            <TUICalendar
-                key = {index}
-                useCreationPopup={true}
-                useDetailPopup={true}
-                height="1000px"
-                view="month"
-                calendars={calendars}
-                schedules={schedules}
-                title = {events.title}
-                //body = {events.body}
-                //location = {events.location}
-                //start = {events.start}
-                //end = {events.end}
-                />
-        ))}
+        <TUICalendar
+        useCreationPopup={true}
+        useDetailPopup={true}
+        height="1000px"
+        view="month"
+        calendars={calendars}
+        schedules={event}
+        //raw = {event}
+        />
         </Layout>
     );
 }
