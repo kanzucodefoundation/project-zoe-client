63321a3c2eadb890f5be9c504f2e76352986e265
Peter Kakoma <kakoma@kanzucode.com>
2022-05-11 09:53:28 +0300

    Fix failing test and add tests to CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import * as redux from 'react-redux';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({ adapter: new Adapter() });
+
+describe('ProjectZoeHome', () => {
+  let spyOnUseSelector;
+  let spyOnUseDispatch;
+  let mockDispatch;
+
+  beforeEach(() => {
+    // Mock useSelector hook
+    spyOnUseSelector = jest.spyOn(redux, 'useSelector');
+    spyOnUseSelector.mockReturnValue([{isLoadingUser: false, user: {}, globalLoader: {}}]);
+
+    // Mock useDispatch hook
+    spyOnUseDispatch = jest.spyOn(redux, 'useDispatch');
+    // Mock dispatch function returned from useDispatch
+    mockDispatch = jest.fn();
+    spyOnUseDispatch.mockReturnValue(mockDispatch);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should render without crashing', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
 });
