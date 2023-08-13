c29468600d84f3892e1ac77fb46d7f7e0a42b4ec
Peter Kakoma <kakoma@kanzucode.com>
2023-08-13 15:57:46 +0300

    Fix cursor skipping to the end during edit

--- src/components/inputs/XTextAreaInput.tsx
+++ src/components/inputs/XTextAreaInput.tsx
@@ -8,23 +8,42 @@ interface IProps {
 }
 
 const XTextAreaInput = ({ name, margin = 'normal', ...props }: TextFieldProps & IProps) => {
-  const [field, meta] = useField({ name });
+  const [field, meta, helpers] = useField({ name });
+
+  const [value, setValue] = React.useState(field.value || ''); // Store the value in state
+  const inputRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (field.value !== value) {
+      setValue(field.value || '');
+    }
+  }, [field.value]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+    helpers.setValue(newValue); // Update Formik's field value
+  };
+
   const error = hasValue(meta.error) ? meta.error : undefined;
   const showError = Boolean(error && meta.touched);
+
   return (
-        <TextField
-            {...field}
-            {...props}
-            fullWidth
-            error={showError}
-            helperText={showError && error}
-            value={field.value || ''}
-            margin={margin}
-            multiline
-            rowsMax="4"
-            rows={4}
-            autoComplete="off"
-        />
+    <TextField
+      {...field}
+      {...props}
+      fullWidth
+      error={showError}
+      helperText={showError && error}
+      value={value}
+      onChange={handleChange}
+      inputRef={inputRef}
+      margin={margin}
+      multiline
+      rowsMax="4"
+      rows={4}
+      autoComplete="off"
+    />
   );
 };
 
--- src/components/inputs/XTextInput.tsx
+++ src/components/inputs/XTextInput.tsx
@@ -12,9 +12,26 @@ const XTextInput = ({
   margin = 'normal',
   ...props
 }: TextFieldProps & IProps) => {
-  const [field, meta] = useField({ name });
+  const [field, meta, helpers] = useField({ name });
+
+  const [value, setValue] = React.useState(field.value || ''); // Store the value in state
+  const inputRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (field.value !== value) {
+      setValue(field.value || '');
+    }
+  }, [field.value]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    setValue(newValue);
+    helpers.setValue(newValue); // Update Formik's field value
+  };
+
   const error = hasValue(meta.error) ? meta.error : undefined;
   const showError = Boolean(error && meta.touched);
+
   return (
     <TextField
       {...field}
@@ -23,7 +40,9 @@ const XTextInput = ({
       fullWidth
       error={showError}
       helperText={showError && error}
-      value={field.value || ''}
+      value={value}
+      onChange={handleChange}
+      inputRef={inputRef}
       autoComplete="nope"
     />
   );
