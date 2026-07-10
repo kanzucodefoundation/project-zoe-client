846003c6105dbe2ac02f166a02b494a12e47ed7b
iamMarybrightEE <marybrightetim@gmail.com>
2026-07-10 08:27:36 +0300

    feat/contact-chips-group-navigation

--- src/modules/contacts/ContactDetail.tsx
+++ src/modules/contacts/ContactDetail.tsx
@@ -292,6 +292,8 @@ const ContactDetail = () => {
                         label={gm.group.name}
                         color="primary"
                         size="medium"
+                        clickable 
+                        onClick={() => navigate(`${localRoutes.groups}/${gm.group.id}`)} 
                       />
                     ),
                 )}
